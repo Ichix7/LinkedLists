866c8336beb4bc9d8e0d1a420bef3825e765880a
Ichix7 <42646489+Ichix7@users.noreply.github.com>
2019-11-04 21:52:26 -0500

    Add files via upload

---
+++ LinkedListConverter.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Linkedlist NumList = new Linkedlist();
+
+            Console.WriteLine("Input number: ");
+            int num = Convert.ToInt32(Console.ReadLine());
+            var Splitnum = num.ToString().Select(x => int.Parse(x.ToString()));
+
+            foreach (var x in Splitnum)
+            {
+                NumList.AddFirst(x);
+            }
+            NumList.printAllNodes();
+            Console.ReadLine();
+        }
+
+    }
+    public class Node
+    {
+        public Node next;
+        public object data;
+    }
+    public class Linkedlist
+    {
+        private Node head;
+
+        public void printAllNodes()
+        {
+            Node current = head;
+            while (current != null)
+            {
+                Console.WriteLine(current.data);
+                current = current.next;
+            }
+        }
+        public void AddFirst(Object data)
+        {
+            Node toAdd = new Node();
+
+            toAdd.data = data;
+            toAdd.next = head;
+
+            head = toAdd;
+        }
+
+    }
+    
+}
---
+++ RadixSort.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections;
+using System.IO;
+namespace csqueue
+{
+    class Class1
+    {
+        enum DigitType
+        {
+            ones = 1,
+            tens = 10,
+            hund = 100
+        }
+
+        static void DisplayArray(int[] n)
+        {
+            for (int x = 0; x <= n.GetUpperBound(0); x++)
+                Console.Write(n[x] + " "); 
+        }
+        static void RSort(Queue[] que, int[] n, DigitType digit)
+        {
+            int snum;
+            for (int x = 0; x <= n.GetUpperBound(0); x++)
+            {
+                if (digit == DigitType.ones)
+                    snum = n[x] % 10;
+
+                else if (digit == DigitType.tens)
+                {
+                    snum = n[x] / 10;
+                    snum = snum % 10;
+                }
+                else
+                    snum  = n[x]/ 100;
+                que[snum].Enqueue(n[x]);
+            }
+        }
+        static void BuildArray(Queue[] que, int[] n)
+        {
+            int y = 0;
+            int x = 0;
+
+            for (x = 0; x <= 9; x++)
+            {
+                while (que[x].Count > 0)
+                {
+                    n[y] = Int32.Parse(que[x].Dequeue().ToString());
+                    y++;
+                }
+            }
+        }
+        static void Main(string[] args)
+        {
+            Queue[] numQueue = new Queue[10];
+            int[] nums = new int[] { 912, 46, 8, 155, 92, 345, 381, 122 };
+            int[] random = new Int32[99];
+            // Display original list
+            for (int i = 0; i < 10; i++)
+                numQueue[i] = new Queue();
+            RSort(numQueue, nums, DigitType.ones);
+            //numQueue, nums, 1
+            BuildArray(numQueue, nums);
+            Console.WriteLine();
+            Console.WriteLine("First pass results: ");
+            DisplayArray(nums);
+            // Second pass sort
+            RSort(numQueue, nums, DigitType.tens);
+            BuildArray(numQueue, nums);
+            Console.WriteLine();
+            Console.WriteLine("Second pass results: ");
+            // Display final results
+            DisplayArray(nums);
+            // Second pass sort
+            RSort(numQueue, nums, DigitType.hund);
+            BuildArray(numQueue, nums);
+            Console.WriteLine();
+            Console.WriteLine("Third pass results: ");
+            // Display final results
+            DisplayArray(nums);
+            Console.WriteLine();
+            Console.Write("Press enter to quit");
+            Console.Read();
+        }
+    }
+}
